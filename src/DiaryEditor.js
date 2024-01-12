fd151fae4168a626830401172519cd4cfeffa0ae
MHLEE12 <coolmh12@gmail.com>
2024-01-12 15:31:44 +0900

    Feat: 입력값 길이 검증 기능 추가
    useRef를 사용해서 입력값인 작성자명과 일기 본문에 최소 작성 길이 검증 기능을 추가했습니다.

--- src/DiaryEditor.js
+++ src/DiaryEditor.js
@@ -1,6 +1,10 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const DiaryEditor = () => {
+  
+  const writerInput = useRef();
+  const contentInput = useRef();
+
   const [state, setState] = useState({
     writer: "",
     content: "",
@@ -15,7 +19,16 @@ const DiaryEditor = () => {
   };
 
   const handleSubmit = () => {
-    console.log(state);
+    if(state.writer.length < 1) {
+      writerInput.current.focus();
+      return;
+    }
+
+    if(state.content.length < 3) {
+      contentInput.current.focus();
+      return;
+    }
+
     alert("저장 성공");
   };
 
@@ -23,13 +36,13 @@ const DiaryEditor = () => {
     <div className="DiaryEditor">
       <h2>오늘의 일기</h2>
       <div>
-        <input name="writer" value={state.writer} onChange={handleChangeState} />
+        <input ref={writerInput} name="writer" value={state.writer} onChange={handleChangeState} />
       </div>
       <div>
-        <textarea name="content" value={state.content} onChange={handleChangeState} />
+        <textarea ref={contentInput} name="content" value={state.content} onChange={handleChangeState} />
       </div>
       <div>
-        기분 점수 :&nbsp;
+        오늘의 점수 :&nbsp;
         <select name="emotion" value={state.emotion} onChange={handleChangeState}>
           <option value={1}>1</option>
           <option value={2}>2</option>
