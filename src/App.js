d85a7c794301a072ae4c89fe82e3ac06a04dd8ce
MHLEE12 <coolmh12@gmail.com>
2024-01-14 22:36:50 +0900

    Comment: 주석 삭제
    더미 데이터를 주석 처리해 놓았었는데 더이상 필요하지 않으므로 삭제함.

--- src/App.js
+++ src/App.js
@@ -3,30 +3,6 @@ import './App.css';
 import DiaryEditor from './DiaryEditor';
 import DiaryList from './DiaryList';
 
-// const dummyList =[
-//   {
-//     id: 1,
-//     writer: "mh1",
-//     content: "안녕 1",
-//     emotion: 5,
-//     createDate: new Date().getTime()
-//   },
-//   {
-//     id: 2,
-//     writer: "mh2",
-//     content: "안녕 2",
-//     emotion: 1,
-//     createDate: new Date().getTime()
-//   },
-//   {
-//     id: 3,
-//     writer: "mh3",
-//     content: "안녕 3",
-//     emotion: 3,
-//     createDate: new Date().getTime()
-//   }
-// ];
-
 function App() {
 
   const [data, setData] = useState([]); // 배열로 초기값 생성
